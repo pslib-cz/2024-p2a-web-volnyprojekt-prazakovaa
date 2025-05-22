0da5dff210bdd1810b920f9079fa36fe176d1930
Nella Pražáková <nella.prazakova.023@pslib.cz>
2025-05-23 00:42:00 +0200

    performance

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,9 @@
-import lightGallery from 'lightgallery';
-import lgZoom from 'lightgallery/plugins/zoom';
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
-
-import 'lightgallery/css/lightgallery.css';
-import 'lightgallery/css/lg-zoom.css';
-import 'lightgallery/css/lg-thumbnail.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const galleryEl = document.getElementById('lightgallery1');
-
-  lightGallery(galleryEl, {
-    selector: 'a',
-    plugins: [lgZoom, lgThumbnail],
-    speed: 500,
-  });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const galleryEl = document.getElementById('lightgallery2');
-
-  lightGallery(galleryEl, {
-    selector: 'a',
-    plugins: [lgZoom, lgThumbnail],
-    speed: 500,
-  });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const galleryEl = document.getElementById('lightgallery3');
-
-  lightGallery(galleryEl, {
-    selector: 'a',
-    plugins: [lgZoom, lgThumbnail],
-    speed: 500,
-  });
-});
+import e from"lightgallery";
+import l from"lightgallery/plugins/zoom";
+import t from"lightgallery/plugins/thumbnail";
+import"lightgallery/css/lightgallery.css";
+import"lightgallery/css/lg-zoom.css";
+import"lightgallery/css/lg-thumbnail.css";
+document.addEventListener("DOMContentLoaded",()=>{let g=document.getElementById("lightgallery1");e(g,{selector:"a",plugins:[l,t],speed:500})}),
+document.addEventListener("DOMContentLoaded",()=>{let g=document.getElementById("lightgallery2");e(g,{selector:"a",plugins:[l,t],speed:500})}),
+document.addEventListener("DOMContentLoaded",()=>{let g=document.getElementById("lightgallery3");e(g,{selector:"a",plugins:[l,t],speed:500})});
